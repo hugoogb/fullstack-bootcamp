aee7ac696aaba0d81099ceb08f6d94d81323a306
Hugoo GB <hugogaben8.02@gmail.com>
2021-05-13 19:07:23 +0200

    part1: started dealing with states

---
+++ part1/example-state/src/App.js
@@ -0,0 +1,109 @@
+import { useState } from 'react'
+import './styles.css'
+
+/*
+const Counter = ({ number }) => {
+  console.log('Counter render')
+  return <h1>{number}</h1>
+}
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+
+  // Equivalent to code above
+  //const contador = useState(0)
+  //const contadorValue = contador[0]
+  //const contadorUpdate = contador[1]
+
+  console.log('render')
+
+  const handleClickIncrease = () => {
+    //setCounter(counter + 1)
+    //setCounter((prevCounter) => prevCounter + 1)
+
+    // Same as the 2 methods above
+    setCounter((prevCounter) => {
+      return prevCounter + 1
+    })
+  }
+
+  const handleClickDecrease = () => {
+    setCounter(counter - 1)
+  }
+
+  const handleClickReset = () => {
+    setCounter(0)
+  }
+
+  const isEven = counter % 2 === 0
+  const messageEvenNotEven = isEven ? 'Is even' : 'Is not even'
+
+  return (
+    <div className='App'>
+      <p>Counter value is:</p>
+      <Counter number={counter} />
+      <p>{messageEvenNotEven}</p>
+      <button onClick={handleClickIncrease}>Increase</button>
+      <button onClick={handleClickDecrease}>Decrease</button>
+      <button onClick={handleClickReset}>Reset</button>
+    </div>
+  )
+}
+*/
+
+const WarningNotUsed = () => {
+  return <h1>Counter not used yet</h1>
+}
+
+const ListOfClicks = ({ clicks }) => {
+  return <p>{clicks.join(', ')}</p>
+}
+
+const App = () => {
+  //const [left, setLeft] = useState(0)
+  //const [right, setRight] = useState(0)
+
+  const [counters, setCounters] = useState({
+    left: 0,
+    right: 0,
+    message: 'Message on state',
+  })
+
+  const [clicks, setClicks] = useState([])
+
+  const handleClickLeft = () => {
+    setCounters({
+      ...counters,
+      left: counters.left + 1,
+    })
+    setClicks((prevClicks) => [...prevClicks, 'L'])
+  }
+
+  const handleClickRight = () => {
+    const newCountersState = {
+      ...counters,
+      right: counters.right + 1,
+    }
+    setCounters(newCountersState)
+    setClicks((prevClicks) => {
+      return [...prevClicks, 'R']
+    })
+  }
+
+  return (
+    <div>
+      {counters.left}
+      <button onClick={handleClickLeft}>left</button>
+      <button onClick={handleClickRight}>right</button>
+      {counters.right}
+      <p>Total clicks: {clicks.length}</p>
+      {clicks.length === 0 ? (
+        <WarningNotUsed />
+      ) : (
+        <ListOfClicks clicks={clicks} />
+      )}
+    </div>
+  )
+}
+
+export default App
