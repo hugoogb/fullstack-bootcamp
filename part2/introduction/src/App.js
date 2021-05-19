c459003bdeac540092e0796b393a2eac4ab38123
Hugoo GB <hugogaben8.02@gmail.com>
2021-05-19 21:38:22 +0200

    part2: modified introduction components

--- part2/introduction/src/App.js
+++ part2/introduction/src/App.js
@@ -1,7 +1,23 @@
-import './App.css'
+import './styles.css'
+import { Note } from './components/Note.js'
 
-function App() {
-  return <div className='App'></div>
+const App = (props) => {
+  const { notes } = props
+
+  if (typeof notes === 'undefined' || notes === null || notes.length === 0) {
+    return 'There are no notes to show'
+  }
+
+  return (
+    <div className='App'>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => {
+          return <Note key={note.id} content={note.content} date={note.date} />
+        })}
+      </ul>
+    </div>
+  )
 }
 
 export default App
